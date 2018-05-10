e33b12e32d8e58a7d48be611d207266cb94cb507
devil-Coder <raj.chandra2016@vitstudent.ac.in>
2018-05-10 16:31:33 +0530

    added working code for testing login in Codepark.in

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var webdriver = require('selenium-webdriver');
 
 var app = express();
 
@@ -12,20 +11,39 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+//Selenium code
 
-var driver = new webdriver.Builder().
-  withCapabilities(webdriver.Capabilities.chrome()).
-  build();
+var webdriver = require('selenium-webdriver');
+//importing webdriver
+const {Builder, By, Key, until} = require('selenium-webdriver');
 
-try {
-    driver.get('http://www.google.com');
-    driver.findElement(webdriver.By.name('q')).sendKeys('simple programmer');
-    driver.findElement(webdriver.By.name('btnG')).click();
-} catch(error){
-  console.log(error);
-} finally {
-    driver.quit();
-}
+/*
+  * Above code is same as below
+  * var webdriver = require('selenium-webdriver'),
+    By = webdriver.By,
+    until = webdriver.until;
+*/
+
+//Initializing driver for chrome
+var driver =new webdriver.Builder()
+    .forBrowser('firefox')
+    .build();
+//Above code can be written as below
+//let driver = await new Builder().forBrowser('chrome').build();
+
+
+(async function example() {
+    try {
+        await driver.get('https://www.codepark.in');
+        await driver.findElement(By.class('btn btn-outline-codepark')).click();
+        await driver.findElement(By.id('loginUsername')).sendKeys(process.env.USERNAME);
+        await driver.findElement(By.id('loginPassword')).sendKeys(process.env.password);
+        await driver.findElement(By.class('btn btn-primary btn-sm')).click();
+        await driver.wait(until.titleIs('CodePark | Dashboard'), 1000);
+    } finally {
+        await driver.quit();
+    }
+})();
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
